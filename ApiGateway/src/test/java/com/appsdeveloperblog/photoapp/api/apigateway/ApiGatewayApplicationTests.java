0a3788774005471cf7555818b6134a1402866261
sonishsinghal <sonishsinghal>
2022-09-12 20:14:52 +0530

    created spring cloud api gateway

---
+++ ApiGateway/src/test/java/com/appsdeveloperblog/photoapp/api/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appsdeveloperblog.photoapp.api.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
